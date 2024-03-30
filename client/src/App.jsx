4680417fde3301cecb43f9fb497c643dc43a9c90
Pratik Satpathy <madengineerps@gmail.com>
2024-03-30 16:11:04 +0530

    server first commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,10 +3,12 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Signin from "./pages/Signin";
 import About from "./pages/About";
+import Header from "./component/Header";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
