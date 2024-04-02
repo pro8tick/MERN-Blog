4de553ca006f65475d94bd5ebbd34b760aa82263
Pratik Satpathy <madengineerps@gmail.com>
2024-04-02 09:25:47 +0530

    model setup

--- server/index.js
+++ server/index.js
@@ -1,4 +1,22 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import { resolve, dirname } from "path";
+import { fileURLToPath } from "url";
+
+// Get directory path of the current module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+dotenv.config({ path: resolve(__dirname, "../.env") });
+mongoose
+  .connect(process.env.mongoURL)
+  .then(() => {
+    console.log("connected to mongo server");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
---
+++ server/models/user.model.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: [true, "User name exists"],
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: [true, "User Email already exists"],
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
